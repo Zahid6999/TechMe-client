37e71b17a7519369626808809e5ec162e477e17e
habllu <zahidhasan6999@gmail.com>
2022-10-27 14:16:39 +0600

    react app environment variables set up

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB6T49n-vhTc57rKUQSXiAeJd_BQprXU9s",
-  authDomain: "techme-web.firebaseapp.com",
-  projectId: "techme-web",
-  storageBucket: "techme-web.appspot.com",
-  messagingSenderId: "442691612302",
-  appId: "1:442691612302:web:9a18482463e8bec02623df"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
